09285788d934b13b3103fa9a0cedaa6c9b1244d7
JuanjeNieto <juanjenieto3107@gmail.com>
2024-01-16 10:07:13 +0100

    arreglar todooooo

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -1,4 +1,4 @@
-// src/Controller/PageController.php
+<?php
 
 namespace App\Controller;
 
@@ -8,41 +8,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    /**
-     * @Route("/about", name="about")
-     */
-    public function about(): Response
+    #[Route('/page', name: 'about')]
+    public function index(): Response
     {
-        return $this->render('page/about.html.twig', [
-            'message' => 'Welcome to the About page!',
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
         ]);
     }
-
-    /**
-     * @Route("/contact", name="contact")
-     */
+    #[Route('/page', name: 'response')]
     public function contact(): Response
     {
         return $this->render('page/contact.html.twig', [
-            'message' => 'Feel free to contact us!',
+            'controller_name' => 'PageController',
         ]);
-    }
-
-    /**
-     * @Route("/redirect", name="redirect")
-     */
-    public function redirectToHome(): Response
-    {
-        return $this->redirectToRoute('home');
-    }
-
-    /**
-     * @Route("/external", name="external_redirect")
-     */
-    public function redirectToExternalSite(): Response
-    {
-        // Replace 'https://example.com' with the actual external website URL
-        $externalUrl = 'https://example.com';
-        return $this->redirect($externalUrl);
     }
 }
